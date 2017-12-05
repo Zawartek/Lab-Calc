e6d679857f1163f095cbc27a12d50ceeddcab2ac
Zawartek <masterdu92@hotmail.fr>
2017-12-05 18:17:21 +0100

    V0.1

--- Calc/Website/site/js/index.js
+++ Calc/Website/site/js/index.js
@@ -1,1 +1,15 @@
 // This is your JS Entry point... You can add as many as JS files you want if you think it is better!
+import axios from 'axios';
+
+function evalFormula(formulaP)
+{
+axios.post('http://localhost:8081/', {
+    formula: formulaP
+})
+.then((response) => {
+    // working
+})
+.catch((error) => {
+    // error
+})
+}
