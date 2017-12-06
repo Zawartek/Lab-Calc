b137ee6b05889ab7e13af5736148ceecf4742652
Zawartek <masterdu92@hotmail.fr>
2017-12-06 13:31:42 +0100

    Correction JS

--- Calc/Website/site/js/index.js
+++ Calc/Website/site/js/index.js
@@ -1,15 +1,26 @@
 // This is your JS Entry point... You can add as many as JS files you want if you think it is better!
 import axios from 'axios';
 
-function evalFormula(formulaP)
-{
-axios.post('http://localhost:8081/', {
-    formula: formulaP
-})
-.then((response) => {
-    // working
-})
-.catch((error) => {
-    // error
-})
+document.getElementById('check').onclick = function() {
+    var formulaP = document.getElementById('formula').value;
+    var retourT = document.getElementById('retour');
+
+    axios.post('http://localhost:8081/', {
+        formula: formulaP
+    })
+    .then((response) => {
+        // working
+        retourT.innerText = "";
+        var expression = eval(formulaP);
+        if (expression === Infinity) {
+            retourT.innerText = "MATH ERROR";
+        }
+        else {
+            retourT.innerText = expression;
+        }
+    })
+    .catch((error) => {
+        // error
+        retourT.innerText = "SYNTAX ERROR";
+    })
 }
