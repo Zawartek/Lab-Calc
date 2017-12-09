92a02b674803d8b72acd9bdf8de91ff3b9dbaf27
Zawartek <masterdu92@hotmail.fr>
2017-12-09 21:12:07 +0100

    Eval replacement

--- Calc/Website/site/js/index.js
+++ Calc/Website/site/js/index.js
@@ -1,43 +1,75 @@
 // This is your JS Entry point... You can add as many as JS files you want if you think it is better!
 import axios from 'axios';
 
-document.getElementById('check').onclick = function() {
+document.getElementById('check').onclick = function () {
     var formulaP = document.getElementById('formula').value;
     var retourT = document.getElementById('retour');
 
     axios.post('http://localhost:8081/', {
         formula: formulaP
     })
-    .then((response) => {
-        // working
-        retourT.innerText = "";
-    })
-    .catch((error) => {
-        // error
-        retourT.innerText = "SYNTAX ERROR";
-    })
+        .then((response) => {
+            // working
+            retourT.innerText = "";
+        })
+        .catch((error) => {
+            // error
+            retourT.innerText = "SYNTAX ERROR";
+        })
 }
 
-document.getElementById('eval').onclick = function() {
+document.getElementById('eval').onclick = function () {
     var formulaP = document.getElementById('formula').value;
     var retourT = document.getElementById('retour');
+    var formulaClean = formulaP.replace(/[^0-9+---*-\/]/g, "");
 
     axios.post('http://localhost:8081/', {
-        formula: formulaP
+        formula: formulaClean
     })
-    .then((response) => {
-        // working
-        retourT.innerText = "";
-        var expression = eval(formulaP);
-        if (expression === Infinity) {
-            retourT.innerText = "MATH ERROR";
-        }
-        else {
-            retourT.innerText = expression;
-        }
-    })
-    .catch((error) => {
-        // error
-        retourT.innerText = "SYNTAX ERROR";
-    })
+        .then((response) => {
+            // working
+            if (eval(formulaClean) === Infinity) {
+                retourT.innerText = "MATH ERROR";
+            }
+            else {
+                var nombres = formulaClean.split(/[+---*-\/]/);
+                var operateurs = formulaClean.split(/[0-9]/);
+                var indiceNb;
+                var indiceOp = 1;
+                var resultat = nombres[0];
+
+                for (indiceNb = 1; indiceNb < nombres.length; indiceNb++) {
+                    resultat = calcul(resultat, nombres[indiceNb], operateurs[indiceOp]);
+                    indiceOp++;
+                }
+
+                retourT.innerText = resultat;
+            }
+        })
+        .catch((error) => {
+            // error
+            retourT.innerText = "SYNTAX ERROR";
+        })
+}
+
+function calcul(param1, param2, operateur) {
+    var resultat;
+    var nombre1 = parseInt(param1);
+    var nombre2 = parseInt(param2);
+
+    switch (operateur) {
+        case "+":
+            resultat = nombre1 + nombre2;
+            break;
+        case "-":
+            resultat = nombre1 - nombre2;
+            break;
+        case "*":
+            resultat = nombre1 * nombre2;
+            break;
+        case "/":
+            resultat = nombre1 / nombre2;
+            break;
+    }
+    return resultat;
 }
